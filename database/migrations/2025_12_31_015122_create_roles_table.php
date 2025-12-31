3a0477de6a6688872dccb4bacec9689f7555a0b1
choudharydakshac <choudharydakshac@gmail.com>
2025-12-31 07:25:32 +0530

    feat: add RBAC database schema with roles, permissions and pivots

---
+++ database/migrations/2025_12_31_015122_create_roles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 50)
+                ->unique()
+                ->comment('System role name e.g. admin, user, staff');
+
+            $table->string('label', 100)
+                ->comment('Human readable role name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
